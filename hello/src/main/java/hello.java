3942deaea13259fe8563e8ffa9d0a1261dcd8ae1
lxz <1565203601@qq.com>
2021-02-17 21:36:17 +0800

    克隆

--- hello/src/main/java/hello.java
+++ hello/src/main/java/hello.java
@@ -2,5 +2,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("1");
          System.out.println("在线更新");
+        System.out.println("克隆");
     }
 }
